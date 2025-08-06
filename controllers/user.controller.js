8534537016b1a3305945f5b2cfe1696e74c4745b
Fahim Alif <fahimalif077@gmail.com>
2025-08-06 21:54:22 +0600

    deploy to prod

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,7 +16,7 @@ export const handleOAuthCallback = async (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // use secure cookie in prod
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
@@ -34,7 +34,7 @@ export const logout = (req, res) => {
  res.cookie('token', '', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // use secure cookie in prod
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
